1680a4bf489ca09885353b884581b2fececbbb91
Vigh Roland <h053675@stud.u-szeged.hu>
2023-03-19 00:00:08 +0100

    Removed bootstrap fixes

--- src/app/shared/spinner/spinner.component.ts
+++ src/app/shared/spinner/spinner.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
-  template: '<mat-spinner [diameter]="24" strokeWidth="4"></mat-spinner>'
+  template: '<mat-spinner [diameter]="19" strokeWidth="3"></mat-spinner>'
 })
 export class SpinnerComponent {}
